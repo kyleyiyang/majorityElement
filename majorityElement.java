7dc8c9cc43b5f7e9eb2b1e49151b8f237bef557e
kyleyiyang <kyleyiyang@gmail.com>
2020-11-01 18:09:14 -0500

    Update majorityElement.java

--- majorityElement.java
+++ majorityElement.java
@@ -14,3 +14,25 @@ class Solution {
         return 1;
     }
 }//very slow
+
+//still slow
+class Solution {
+    public int majorityElement(int[] nums) {
+        var mySet = Set.of(nums);
+        for (int[] a:mySet) {
+            for (int i=0;i<a.length;i++){
+                int count=0;
+                for (int j=0;j<nums.length;j++) {
+                    if (a[i]==nums[j]) {
+                        count++;
+                    }
+                }
+                if (count>Math.floor(nums.length/2)) {
+                    return a[i];
+                }
+            }
+            
+        }
+        return 0;
+    }
+}
