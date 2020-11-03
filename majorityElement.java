a1072b3e81160a429b548c85a3845b4cf4b5e936
kyleyiyang <kyleyiyang@gmail.com>
2020-11-03 17:15:28 -0500

    Update majorityElement.java

--- majorityElement.java
+++ majorityElement.java
@@ -127,3 +127,64 @@ class Solution {
         //}
     }
 }
+
+// 46 / 46 test cases passed, but took too long.
+class Solution {
+    public int majorityElement(int[] nums) {
+        sort(nums,0,nums.length-1);
+        for (int i=0;i<nums.length; i++) {
+            //System.out.println(nums[i]);
+            int count=1;
+            while (i<nums.length-1 && nums[i]==nums[i+1]) {
+                count++;
+                i++;
+            }
+            //System.out.println("count="+count);
+            if (count>Math.floor(nums.length/2)) {
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+    /*public static void sort(int[] arr, int p, int q) {
+        if (p<q) {
+            int i = partition(arr,p,q);
+            sort(arr,p,i-1);
+            sort(arr,i+1,q);
+        }
+    }*/
+    public static void sort(int[] arr, int p, int q) {
+       if (p<q) {
+            /*int k = arr[q];
+            int j = q;
+            for (int i=q-1; i>=p; i--) {
+                if (arr[i]>=k) {
+                    j--;
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                    
+                }
+            }
+            arr[q]=arr[j-1];
+            arr[j-1]=k;
+            sort(arr, p, j-1);
+            sort(arr, j+1, q);*/
+           int x = arr[q];
+            int i = p-1;
+            for (int j=p; j<q; j++) {
+                if (arr[j]<=x) {
+                    i++;
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i]=temp;
+                }
+            }
+            arr[q]=arr[i+1];
+            arr[i+1]=x;
+            sort(arr, p, i);
+            sort(arr, i+2, q);
+        //return i+1;
+        }
+    }
+}
