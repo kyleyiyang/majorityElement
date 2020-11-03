ba575216bc66398ab7018856b1b030ea3914961a
kyleyiyang <kyleyiyang@gmail.com>
2020-11-02 23:40:47 -0500

    Update majorityElement.java

--- majorityElement.java
+++ majorityElement.java
@@ -36,3 +36,40 @@ class Solution {
         return 0;
     }
 }
+
+//  quick sort
+class Solution {
+    public int majorityElement(int[] nums) {
+        System.out.println(nums.length);
+        sort(nums,0,nums.length-1);
+        for (int i=0;i<nums.length; i++) {
+            System.out.println(nums[i]);
+            int count=1;
+            while (i<nums.length-1 && nums[i]==nums[i+1]) {
+                count++;
+                i++;
+            }
+            System.out.println("count="+count);
+            if (count>Math.floor(nums.length/2)) {
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+    public static void sort(int[] arr, int p, int q) {
+        if (p<q) {
+            int k = arr[q];
+            int j = q;
+            for (int i=q-1; i>=0; i--) {
+                if (arr[i]>k) {
+                    arr[j]=arr[i];
+                    j=i;
+                }
+            }
+            arr[j]=k;
+            sort(arr, j+1, q);
+            sort(arr, p, j-1);
+        }
+    }
+}
+
